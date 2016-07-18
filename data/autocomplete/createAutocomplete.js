74382836e2b07607abfb26106be0aa0202c8aa7b
ErikNovak <novak.erik@ijs.si>
2016-07-18 13:45:29 +0200

    Made new database generation functions.

---
+++ data/autocomplete/createAutocomplete.js
@@ -0,0 +1,59 @@
+﻿/**
+﻿ * Creates the files used for input autocompletion.
+ */
+
+var qm = require('qminer');
+var fs = require('fs');
+
+console.log('Building base...');
+var base = new qm.Base({
+    mode:   'openReadOnly',
+    dbPath: '../database/lecturesTest/'
+});
+
+console.log('Preparing writing files...');
+// initialize the autocompletion files
+var authorsFs       = fs.createWriteStream('authors.aut', 'utf8');
+var categoriesFs    = fs.createWriteStream('categories.aut', 'utf8');
+var languagesFs     = fs.createWriteStream('languages.aut', 'utf8');
+var organizationsFs = fs.createWriteStream('organizations.aut', 'utf8');
+
+// write authors in the data
+console.log('Writing authors...');
+var authors = base.store("Authors").allRecords;
+for (var AuthorN = 0; AuthorN < authors.length; AuthorN++) {
+    authorsFs.write(authors[AuthorN].name + '\n');
+}
+authorsFs.end();
+
+// write categories in the data
+console.log('Writing categories...');
+var categories = base.store("Categories").allRecords;
+for (var CatN = 0; CatN < categories.length; CatN++) {
+    categoriesFs.write(categories[CatN].title + '\n');
+}
+categoriesFs.end();
+
+// write languages in the data
+console.log('Writing languages...');
+var lectures  = base.store("Lectures").allRecords;
+var languageHt = new qm.ht.StrIntMap();
+for (var LectureN = 0; LectureN < lectures.length; LectureN++) {
+    var language = lectures[LectureN].language;
+    if (!languageHt.hasKey(language)) {
+        languagesFs.write(language + '\n');
+        languageHt.put(language, 1);
+    }
+}
+languagesFs.end();
+
+// write categories in the data
+console.log('Writing organizations...');
+var organizations = base.store("Organizations").allRecords;
+for (var OrgN = 0; OrgN < organizations.length; OrgN++) {
+    organizationsFs.write(organizations[OrgN].name + '\n');
+}
+organizationsFs.end();
+
+console.log('Finished');
+base.close();
--- data/database/createBase.js
+++ data/database/createBase.js
@@ -1,81 +1,178 @@
 ﻿/**
- * Creates the base containing the videolectures dataset. 
- * It takes the data from the videolectures .csv file and creates the base.
+ * Creates the database containing the videolectures dataset.
+ * It takes the data stored in rawdata.
  * Schema is defined in the schema.json file.
- **/
+ */
 
+var fs = require('fs');
 var qm = require('qminer');
-var fs = qm.fs;
 
-// csv file 
-var lectures = new fs.FIn('videolectures-20160216.csv');
+console.log('Reading raw files...');
+// get all the raw data
+// these are arrays
+var authors       = JSON.parse(fs.readFileSync('rawdata/authors.json', 'utf8'));
+var lectures      = JSON.parse(fs.readFileSync('rawdata/lectures.json', 'utf8'));
+var organizations = JSON.parse(fs.readFileSync('rawdata/organizations.json', 'utf8'));
 
-// the base
+// these are json objects
+// TODO: when API allows, use API to get the data
+var categories = JSON.parse(fs.readFileSync('rawdata/categories.json', 'utf8'))[0];
+var edges      = JSON.parse(fs.readFileSync('rawdata/edges.json', 'utf8'))[0];
+
+console.log('Files read');
+
+// base construction
 var base = new qm.Base({
     mode:       "createClean",
     schemaPath: "schema.json",
-    dbPath:     "lectures/"
-})
+    dbPath:     "lecturesTest/"
+});
 
-// the first line is skipped, it contains the column names
-lectures.readLine();
+// 1. import organizations
+console.log('Importing organizations...');
+for (var OrgN = 0; OrgN < organizations.length; OrgN++) {
+    var organization = organizations[OrgN];
+    var record = {
+        "index":   organization.id.toString(),
+        "name":    organization.name,
+        "city":    organization.city    != "" ? organization.city    : null,
+        "country": organization.country != "" ? organization.country : null
+    };
+    base.store('Organizations').push(record);
+}
+console.log('Organizations imported');
+console.log("Number of organizations", base.store('Organizations').length);
 
-// import the lectures in the base
-console.log("Storing data in the base...");
-var lCounter = 0;
-while (!lectures.eof) {
-    var lecture = lectures.readLine().split(';');
-    var index        = lecture[1];
-    var title        = lecture[2];
-    var author       = lecture[3];
-    var gender       = lecture[4];
-    var organization = lecture[5] != '' ? lecture[5] : null;
-    var language     = lecture[6] != '' ? lecture[6] : null;
-    var published    = lecture[7] != '' ? lecture[7] + "T00:00:00" : null;
-    
-    var duration = null;
-    if (lecture[8] != '') {
-        var digital = lecture[8].split(':');
-        // set duration
-        duration = 0;
-        for (var h = 0; h < digital.length; h++) {
-            duration += parseInt(digital[h]) * Math.pow(60, digital.length - h - 1) * 1000;
-        }
+// 2. import categories
+console.log('Import categories...');
+var categoryKeys = Object.keys(categories);
+for (var CatN = 0; CatN < categoryKeys.length; CatN++) {
+    var catKey   = categoryKeys[CatN];
+    var category = categories[catKey];
+
+    var record = {
+        "index":       catKey.slice(1),
+        "title":       category.text.title,
+        "path":        category.url.split('/'),
+        "wiki":        category.refs      ? category.refs.wiki : null,
+        "description": category.text.desc ? category.text.desc : null
     }
-    
-    var views = lecture[9] != '' ? parseInt(lecture[9]) : null;
+    base.store('Categories').push(record);
+}
+console.log('Categories imported');
+console.log("Number of categories", base.store('Categories').length);
 
-    var categories = null;
-    if (lecture[10] != '') {
-        var topics = lecture[10].replace(/[_]+/g, ' ').split(/[,;\/]+/g);
-        // create an array
-        categories = [];
-        for (t = 0; t < topics.length; t++) {
-            if (categories.indexOf(topics[t]) == -1) {
-                categories.push(topics[t]);
+// 3. add categories parent join
+console.log('Adding joins to categories...');
+for (var CatN = 0; CatN < categoryKeys.length; CatN++) {
+    var catKey   = categoryKeys[CatN];
+    var category = categories[catKey];
+
+    var recId    = catKey.slice(1);
+
+    if (category.edges && base.store('Categories').recordByName(recId)) {
+        var joinId = category.edges.parent.slice(1);
+        base.store('Categories').recordByName(recId).$addJoin('parent',
+            base.store('Categories').recordByName(joinId)
+        );
+    }
+}
+console.log('Finished adding joins to categories');
+
+// 3. import authors
+console.log('Importing Authors...');
+for(var AutN = 0; AutN < authors.length; AutN++) {
+    var author = authors[AutN];
+    var record = {
+        "index":  author.id.toString(),
+        "name":   author.name,
+        "title":  author.title  != '' ? author.title  : null,
+        "gender": author.gender != '' ? author.gender : null,
+        "slug":   author.slug   != '' ? author.slug   : null,
+    }
+    base.store('Authors').push(record);
+
+    var orgRef = author.organization;
+    if (orgRef && base.store('Organizations').recordByName(orgRef.id.toString())) {
+        base.store('Authors').recordByName(author.name).$addJoin('worksAt',
+            base.store('Organizations').recordByName(orgRef.id.toString())
+        );
+    }
+}
+console.log('Authors imported');
+console.log('Number of authors', base.store('Authors').length);
+
+// 4. import lectures
+console.log('Import Lectures...');
+for(var LectN = 0; LectN < lectures.length; LectN++) {
+    var lecture = lectures[LectN];
+    var time    = Date.parse(lecture.time) > 0 ? lecture.time.slice(0, 19) : null;
+    var record = {
+        "index":       lecture.id.toString(),
+        "slug":        lecture.slug,
+        "recorded":    time,
+        "type":        lecture.type,
+        "title":       lecture.title,
+        "description": lecture.description != '' ? lecture.description : null,
+        "language":    lecture.language,
+        "duration":    lecture.duration,
+        "views":       lecture.view_ctr
+    }
+    base.store('Lectures').push(record);
+
+    // join the presenters
+    var presenters = lecture.authors;
+    if (presenters) {
+        for (var PresN = 0; PresN < presenters.length; PresN++) {
+            var presenter = presenters[PresN];
+            if (base.store('Authors').recordByName(presenter)) {
+                base.store('Lectures').recordByName(lecture.id.toString()).$addJoin(
+                    'presenters', base.store('Authors').recordByName(presenter)
+                )
             }
         }
     }
-    
-    var keywords    = lecture[11] != '' ? lecture[11] : null;
-    var description = lecture[12] != '' ? lecture[12] : null;
-    
-    base.store("Lectures").push({
-        "index"         : index,
-        "title"         : title,
-        "author"        : author,
-        "gender"        : gender,
-        "organization"  : organization,
-        "language"      : language,
-        "published"     : published,
-        "duration"      : duration,
-        "views"         : views,
-        "categories"    : categories,
-        "keywords"      : keywords,
-        "description"   : description
-    });
-
-    lCounter++;
 }
-console.log("Imported " + lCounter + " lectures!");
+console.log('Lectures imported');
+console.log('Number of lectures', base.store('Lectures').length);
+
+// 5. add lectures categories join
+console.log('Joining Categories and Lectures...');
+var edgeKeys = Object.keys(edges);
+for (var EdgeN = 0; EdgeN < edgeKeys.length; EdgeN++) {
+    var edgeKey = edgeKeys[EdgeN];
+    var lecture = edges[edgeKey];
+
+    var lectureId = edgeKey.slice(1);
+
+    if (lecture.categories && base.store('Lectures').recordByName(lectureId)) {
+        var lectCategoryKeys = Object.keys(lecture.categories);
+        for (var CatN = 0; CatN < lectCategoryKeys.length; CatN++) {
+            var catId = lectCategoryKeys[CatN].slice(1);
+            if (base.store('Categories').recordByName(catId)) {
+                base.store('Lectures').recordByName(lectureId).$addJoin('categories',
+                    base.store('Categories').recordByName(catId)
+                );
+            }
+        }
+    }
+}
+console.log('Finished Joining Categories and Lectures');
+
+// 6. add lectures hasParent join
+console.log('Adding Lectures parent Join...');
+for(var LectN = 0; LectN < lectures.length; LectN++) {
+    var lecture = lectures[LectN];
+
+    // join the presenters
+    if (lecture.parent) {
+        var parentId = lecture.parent.id.toString();
+        if (base.store('Lectures').recordByName(parentId)) {
+            base.store('Lectures').recordByName(lecture.id.toString()).$addJoin(
+                'parent', base.store('Lectures').recordByName(parentId)
+            )
+        }
+    }
+}
+console.log('Finished Joining parent Lectures');
 base.close();
